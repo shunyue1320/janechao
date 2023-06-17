dbf304b52b577451c23dc716b9d40d73574082d6
shunyue1320 <tsunyue1320@gmail.com>
2023-06-17 22:38:30 +0800

    chore: food recommend app

--- janechao/ContentView.swift
+++ janechao/ContentView.swift
@@ -7,15 +7,67 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+
+
 struct ContentView: View {
+    let food = Food.examples
+    @State private var selectedFood: Food?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Image("dinner")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                
+            Text("今天吃什么？")
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood!.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.green)
+                    .id(selectedFood!.name)
+                    .transition(.asymmetric(
+                        insertion: .opacity
+                                    .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                        removal: .opacity
+                                    .animation(.easeInOut(duration: 0.4))))
+            }
+            
+            Button {
+                selectedFood = food.shuffled().first { $0 != selectedFood }
+            } label: {
+                Text(selectedFood == .none ? "告诉我" : "换一个").frame(width: 200)
+                    .animation(.none, value: selectedFood)
+                    .transformEffect(.identity)
+            }.padding(.bottom, -15)
+            
+            Button {
+                selectedFood = .none
+            } label: {
+                Text("重置").frame(width: 200)
+            }.buttonStyle(.bordered)
         }
         .padding()
+        .frame(maxHeight: .infinity)
+        .background(Color(.secondarySystemBackground))
+        .font(.title)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .animation(.easeInOut(duration: 0.6), value: selectedFood)
     }
 }
 
