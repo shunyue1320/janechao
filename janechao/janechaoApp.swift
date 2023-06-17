93e84b4319f695152e626337ca99b33359f1a98f
shunyue1320 <tsunyue1320@gmail.com>
2023-06-17 22:01:50 +0800

    Initial Commit

---
+++ janechao/janechaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  janechaoApp.swift
+//  janechao
+//
+//  Created by 郭健波 on 2023/6/17.
+//
+
+import SwiftUI
+
+@main
+struct janechaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
